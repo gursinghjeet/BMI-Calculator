5986fa03a412d181d85def9a9448eee790c24302
Gurjeet Singh <hit19ece.gurjeetsingh@gmail.com>
2023-09-20 00:50:19 +0530

    Updated BackgroundImage

--- script.js
+++ script.js
@@ -7,7 +7,23 @@ form.addEventListener("submit",function(e){
     const weight = document.querySelector("#weight").value;
     console.log(height);
     console.log(weight);
+    if(height<0 || isNaN(height) || height === ''){
+        result.innerHTML = `Please Enter Valid height.`
+    }else if(weight<0 || isNaN(weight) || weight === ''){
+        result.innerHTML = `Please Enter Valid weight.`
+    }else{
     const bmi = (weight/((height * height) / 10000)).toFixed(2);
     console.log(bmi);
+    
     result.innerHTML = `${bmi}`;
+    
+    
+      if(bmi <= 18.6){
+        description.innerHTML = `You Are Under Weight`;
+      }else if(bmi < 24.9 &&  bmi> 18.6){
+        description.innerHTML = `Congratulation You Are Fit`;
+      }else{
+         description.innerHTML = `You Are Over Weight`;
+      }
+    }
 });
